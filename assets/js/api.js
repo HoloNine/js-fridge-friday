df62b2cf37cc187c6e9a7d0b96b53ae6e3a176e0
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-01-08 20:26:53 +0000

    feat: add getMeal function

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,15 +2,48 @@ const mealAPI = () => {
   const searchRecipeFormInput = $(".search-recipe-form-input");
   const searchRecipeFormButton = $(".search-recipe-form-button");
 
+  const buildApiUrl = (props) => {
+    const { query, value } = props;
+
+    const apiKey = 1;
+    const rootUrl = `https://www.themealdb.com/api/json/v1/${apiKey}`;
+
+    switch (query) {
+      case "search":
+        return `${rootUrl}/${query}.php?${query.at(0)}=${value}`;
+        break;
+      case "random":
+        return `${rootUrl}/${query}.php`;
+        break;
+      case "ingredient":
+        return `${rootUrl}/filter.php?${query.at(0)}=${value}`;
+        break;
+    }
+  };
+
+  const getMeal = (ingredient) => {
+    const url = buildApiUrl({ query: "ingredient", value: ingredient });
+    const method = "GET";
+
+    $.ajax({ url, method }).then((response) => {
+      const { meals } = response;
+
+      console.log(meals);
+    });
+  };
+
+  /**
+   * @description search recipe form button handler
+   */
   const searchRecipeFormButtonHandler = (event) => {
     event.preventDefault();
 
-    /**
-     * store input value in a constant that can be used later in our handler function
-     */
-    const searchRecipeFormInputValue = searchRecipeFormInput.val();
+    // store input value in a constant that can be used later in our callback function
+    const ingredient = searchRecipeFormInput.val();
+    if (!ingredient) return;
 
-    console.log({ searchRecipeFormInputValue });
+    // if the input form element returns a value then the function getMeal will be executed
+    getMeal(ingredient);
   };
 
   searchRecipeFormButton.on("click", searchRecipeFormButtonHandler);
