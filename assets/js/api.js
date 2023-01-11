fa8529d737f24e54df6f180382c8f656360446fe
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-01-11 20:11:17 +0000

    feat: add redirect function if there is no error on the api side

--- assets/js/api.js
+++ assets/js/api.js
@@ -8,68 +8,20 @@ const popularRecipesWrapper = $(".popular-recipes-wrapper");
 const getMeal = (ingredient) => {
   const url = buildApiUrl({ query: "ingredient", value: ingredient });
   const method = "GET";
+  const success = (response) => {
+    if (response.meals) {
+      window.location.href = "./search.html";
+    }
+  };
 
-  $.ajax({ url, method }).then((response) => {
+  $.ajax({ url, method, success }).then((response) => {
     const { meals } = response;
   });
 };
 
-const buildRandomMealHero = (props) => {
-  const { idMeal, strMeal, strInstructions, strMealThumb } = props;
-
-  const strInstructionsSubstring = strInstructions.substring(0, 240);
-
-  const heroBackground = $(".hero-background");
-  const heroTitle = $(".hero-title");
-  const heroText = $(".hero-text");
-  const heroButton = $(".hero-button");
-
-  heroBackground.css({
-    background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)), url("${strMealThumb}")`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-  });
-
-  heroTitle.text(strMeal);
-  heroText.text(`${strInstructionsSubstring}...`);
-  heroButton.attr("id", idMeal);
-};
-
-const buildRandomMealCard = (props) => {
-  const { idMeal, strMeal, strInstructions, strMealThumb } = props;
-
-  const strInstructionsSubstring = strInstructions.substring(0, 120);
-
-  // Image
-  const popularRecipeCard = $("<div>").addClass("popular-recipes-card");
-  const popularRecipeImage = $("<div>").addClass("popular-recipe-card-image");
-  const popularRecipeImageTag = $("<img>").attr("src", strMealThumb);
-
-  const popularRecipeCardBody = $("<div>").addClass("popular-recipe-card-body");
-  const popularRecipeCardTitle = $("<h5>")
-    .addClass("popular-recipe-card-title")
-    .text(strMeal);
-
-  const popularRecipeCardDescription = $("<p>")
-    .addClass("popular-recipe-card-description")
-    .text(`${strInstructionsSubstring}...`);
-
-  const popularRecipeButton = $("<button>")
-    .addClass("button button-primary")
-    .attr("id", idMeal)
-    .text("See More");
-
-  popularRecipeImage.append(popularRecipeImageTag);
-  popularRecipeCard.append(popularRecipeImage);
-
-  popularRecipeCardBody.append(popularRecipeCardTitle);
-  popularRecipeCardBody.append(popularRecipeCardDescription);
-  popularRecipeCardBody.append(popularRecipeButton);
-  popularRecipeCard.append(popularRecipeCardBody);
-
-  popularRecipesWrapper.append(popularRecipeCard);
-};
-
+/**
+ * @description get random meals from the api, render the popular items and the hero
+ */
 const getRandomMeals = (numberOfMeals) => {
   const url = buildApiUrl({ query: "random", value: null });
   const method = "GET";
