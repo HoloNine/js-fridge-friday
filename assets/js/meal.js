82867b3a0613d7baa49a040c527f0555fbbcf508
Kirsten <kh_2015@yahoo.com>
2023-01-12 20:05:13 +0000

    readme text

---
+++ assets/js/meal.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const searchMeal = getSearchMeal();
+  buildMealHero(searchMeal);
+
+  const { idMeal } = searchMeal;
+
+  const ingredientWrapper = $(".meal-ingredients-list");
+  const mealKeys = Object.keys(searchMeal);
+
+  const mealIngredients = [];
+
+  mealKeys.forEach((key) => {
+    if (key.startsWith("strIngredient")) {
+      mealIngredients.push(key);
+    }
+  });
+
+  console.log(mealIngredients);
+  getMealId(idMeal)
+});
+
+
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const searchMealsHistory = getSearchHistory();
+  const method = "GET";
 
-  searchMealsHistory.forEach((element) => {
+  searchMealsHistory.forEach((element, index) => {
     const { idMeal } = element;
 
     const url = buildApiUrl({ query: "lookup", value: idMeal });
-    const method = "GET";
-    $.ajax(url, method).then((response) =>
-      buildRandomMealCard(response.meals[0])
-    );
+
+    if (index < 6) {
+      $.ajax(url, method).then((response) =>
+        buildRandomMealCard(response.meals[0])
+      );
+    }
   });
 });
