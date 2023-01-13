2044f232c76de90e4271e8b20d0081bce6518147
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-01-13 10:24:53 +0000

    feat: add ingredients quantity in meal.html

--- assets/js/meal.js
+++ assets/js/meal.js
@@ -10,21 +10,30 @@ document.addEventListener("DOMContentLoaded", () => {
   const mealKeys = Object.keys(searchMeal);
 
   const mealIngredients = [];
+  const mealValues = [];
 
   mealKeys.forEach((key) => {
     if (key.startsWith("strIngredient")) {
       mealIngredients.push(key);
     }
+    if (key.startsWith("strMeasure")) {
+      mealValues.push(key);
+    }
   });
 
-  mealIngredients.forEach((ingredient) => {
+  mealIngredients.forEach((ingredient, index) => {
     if (searchMeal[ingredient] !== null && searchMeal[ingredient] !== "") {
       const ingredientEl = $("<div>").addClass("ingredient");
       const spanEl = $("<span>").addClass("circle");
       const spanTextEl = $("<span>").addClass("text");
+      const ingredientVal = $("<span>").addClass("value");
+      ingredientVal.text(searchMeal[mealValues[index + 1]]);
+
       spanTextEl.text(`${searchMeal[ingredient]}`);
       ingredientEl.append(spanEl);
       ingredientEl.append(spanTextEl);
+      ingredientEl.append(ingredientVal);
+
       ingredientWrapper.append(ingredientEl);
     }
   });
